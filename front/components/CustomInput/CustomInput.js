b939d0db176924a6eff4b0a1753ecfc2d22db6e2
Do-it-chu <103571758+Do-it-chu@users.noreply.github.com>
2022-06-15 01:06:03 +0900

    FEAT: input + 유효성체크
    input 입력값 및 유효성 체크

--- front/components/CustomInput/CustomInput.js
+++ front/components/CustomInput/CustomInput.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState }from "react";
 // nodejs library to set properties for components
 import PropTypes from "prop-types";
 // nodejs library that concatenates classes
@@ -10,6 +10,7 @@ import InputLabel from "@material-ui/core/InputLabel";
 import Input from "@material-ui/core/Input";
 
 import styles from "styles/jss/nextjs-material-kit/components/customInputStyle.js";
+import LoginPage from "../../pages/login";
 
 const useStyles = makeStyles(styles);
 
@@ -24,7 +25,8 @@ export default function CustomInput(props) {
     error,
     white,
     inputRootCustomClasses,
-    success
+    success,
+    saveInfo
   } = props;
 
   const labelClasses = classNames({
@@ -53,31 +55,81 @@ export default function CustomInput(props) {
   } else {
     formControlClasses = classes.formControl;
   }
-  return (
-    <FormControl {...formControlProps} className={formControlClasses}>
+
+  // 신랑 신부 예식장 정보 등 입력 값 변수 저장
+  const [brideName, setBrideName] = useState("");
+  const [groomName, setGroomName] = useState("");
+  const [bridePhone, setBridePhone] = useState("");
+  const [groomPhone, setGroomPhone] = useState("");
+  const [brideAccount, setBrideAccount] = useState("");
+  const [groomAccount, setGroomAccount] = useState("");
+
+  
+  
+    // const inputChangeSave = (e) => {
+    //   const inputValue = e.target.value;
+    //   const userInfo = props.userInfo;
+    //   props.saveInfo(inputValue, userInfo);
+    // }
+  
+    
+  
+    
+    
+    return (
+      <FormControl {...formControlProps} className={formControlClasses}>
       {labelText !== undefined ? (
         <InputLabel
-          className={classes.labelRoot + " " + labelClasses}
-          htmlFor={id}
-          {...labelProps}
+        className={classes.labelRoot + " " + labelClasses}
+        htmlFor={id}
+        {...labelProps}
         >
           {labelText}
         </InputLabel>
       ) : null}
+      {/* <LoginPage onChange={inputChangeSave}/> */}
+      
       <Input
-      onChange={ (e) => console.log(e.target.value)}
+      // onChange={(e) => { console.log(e.target.value) }}
+      
+      onChange={ (e) => {
+        if (id == 'setBrideName') {
+          // props.saveInfo().brideAccount(e.target.value);
+          setBrideName(e.target.value);
+        }else if(id == 'setGroomName') {
+          // props.saveInfo.groomName(e.target.value)
+          //props.saveInfo(setGroomName(e.target.value));
+          setGroomName(e.target.value);
+        }else if(id == 'setBridePhone'){
+          // props.saveInfo.bridePhone(e.target.value)
+          //props.saveInfo(setBridePhone(e.target.value));
+          setBridePhone(e.target.value);
+        }else if(id == 'setGroomPhone'){
+          // props.saveInfo.groomPhone(e.target.value)
+          //props.saveInfo(setGroomPhone(e.target.value));
+          setGroomPhone(e.target.value)
+        }else if(id == 'setBrideAccount'){
+          // props.saveInfo.brideAccount(e.target.value)
+          //props.saveInfo(setBrideAccount(e.target.value));
+          setBrideAccount(e.target.value)
+        }else if(id == 'setGroomAccount'){
+          // props.saveInfo.groomAccount(e.target.value)
+          //props.saveInfo(setGroomAccount(e.target.value));
+          setGroomAccount(e.target.value)
+        }
+      }}
         classes={{
           input: inputClasses,
           root: marginTop,
           disabled: classes.disabled,
           underline: underlineClasses,
           
-          
-          
         }}
         id={id}
         {...inputProps}
         />
+        {console.log("커스텀로그"+id+"---"+brideName+groomName+bridePhone+groomPhone+brideAccount+groomAccount)}
+        {console.log("로그잼 " + inputProps)}
         
     </FormControl>
   );
--- front/pages/login.js
+++ front/pages/login.js
@@ -1,19 +1,13 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Datetime from "react-datetime";
 // @material-ui/core components
 import { makeStyles } from "@material-ui/core/styles";
-import InputAdornment from "@material-ui/core/InputAdornment";
-import Icon from "@material-ui/core/Icon";
 import Dialog from "@material-ui/core/Dialog";
 import DialogTitle from "@material-ui/core/DialogTitle";
 import DialogContent from "@material-ui/core/DialogContent";
 import DialogActions from "@material-ui/core/DialogActions";
 import Slide from "@material-ui/core/Slide";
 // @material-ui/icons
-import People from "@material-ui/icons/People";
-import Account from "@material-ui/icons/Payment";
-import Phone from "@material-ui/icons/Phone";
-import Info from "@material-ui/icons/InfoOutlined";
 import IconButton from "@material-ui/core/IconButton";
 import LibraryBooks from "@material-ui/icons/LibraryBooks";
 import Close from "@material-ui/icons/Close";
@@ -29,9 +23,7 @@ import Card from "components/Card/Card.js";
 import CardBody from "components/Card/CardBody.js";
 import CardHeader from "components/Card/CardHeader.js";
 import CardFooter from "components/Card/CardFooter.js";
-import CustomInput from "components/CustomInput/CustomInput.js";
 import UploadImages from "./uploadImages";
-import Address from "./address";
 
 import styles from "styles/jss/nextjs-material-kit/pages/loginPage.js";
 import { FormControl, InputLabel, TableBody } from "@material-ui/core";
@@ -48,12 +40,69 @@ export default function LoginPage(props) {
   const [cardAnimaton, setCardAnimation] = React.useState("cardHidden");
   const [classicModal, setClassicModal] = React.useState(false);
 
+  
+    
+    const [weddingHall, setWeddingHall] = useState("");
+    const [weddingDate, setWeddingDate] = useState("");
+    // const [customInfo, setCustomInfo] = React.useState([]);
+
+    const onChangeBrideName = (e) => setBrideName(e.target.value);
+    const onChanGroomName = (e) => setGroomName(e.target.value);
+    const onChangeBridePhone = (e) => setBridePhone(e.target.value);
+    const onChangeGroomPhone = (e) => setGroomPhone(e.target.value);
+    const onChangeBrideAccoun = (e) => setBrideAccount(e.target.value);
+    const onChangeGroomAccount = (e) => setGroomAccount(e.target.value);
+    const onChangeWeddingHall = (e) => setWeddingHall(e.target.value);
+  
+    
+
+  // const custom = customInfo.map(info => (
+
+  // ));
+
+  const inputSave = () =>{
+    setCartIsShown(true);
+  }
+
+
+
   setTimeout(function () {
     setCardAnimation("");
   }, 700);
   const classes = useStyles();
   const { ...rest } = props;
+
+
+  
+  //신랑 신부 예식장 정보 등 입력 값 변수 저장
+  const [brideName, setBrideName] = useState("");
+  const [groomName, setGroomName] = useState("");
+  const [bridePhone, setBridePhone] = useState("");
+  const [groomPhone, setGroomPhone] = useState("");
+  const [brideAccount, setBrideAccount] = useState("");
+  const [groomAccount, setGroomAccount] = useState("");
+  
+  const handlePress = (e) => {
+    console.log("handlePress");
+    const regex = /^[0-9\b -]{0,13}$/;
+    if (regex.test(e.target.value)) {
+      console.log("handlePress2");
+      setBridePhone(e.target.value);
+      }
+    }
+
+  // useEffect(() => {
+  //   if (setBridePhone.length === 10) {
+  //     setBridePhone(bridePhone.replace(/(\d{3})(\d{3})(\d{4})/, '$1-$2-$3'));
+  //   }
+  //   if (bridePhone.length === 13) {
+  //     setBridePhone(bridePhone.replace(/-/g, '').replace(/(\d{3})(\d{4})(\d{4})/, '$1-$2-$3'));
+  //   }
+  // }, [bridePhone]);
+  
+
   return (
+    <>
     <div>
       <Header
         absolute
@@ -83,10 +132,10 @@ export default function LoginPage(props) {
                   </p>
                   <CardBody>
                     <p className={classes.divider}>신부 측</p>
-                    <CustomInput
+                    {/* <CustomInput
                       labelText="Name..."
-                      id="name"
-                      onChange={ (e) => console.log(e.target.value)}
+                      id="setBrideName"
+                      saveInfo={userInfo.brideName}
                       formControlProps={{
                         fullWidth: true,
                       }}
@@ -94,15 +143,19 @@ export default function LoginPage(props) {
                         type: "text",
                         endAdornment: (
                           <InputAdornment position="end">
-                             
                             <People className={classes.inputIconsColor} />
                           </InputAdornment>
                         ),
                       }}
-                    />
-                    <CustomInput
+                    /> */}
+                    <label>신부 성함</label>
+                    <input type="text" placeholder="이름" id="brideName" onChange={(e) => {setBrideName(e.target.value), console.log("brideName"+brideName)}}/>
+                    {/* <input type="text" name="name" onChange={this.handleChangeInput} value={this.state.input}/> */}
+                    {/* <CustomInput
                       labelText="Phone..."
-                      id="phone"
+                      id="setBridePhone"
+                      saveInfo={userInfo.bridePhone}
+                      onClick={ (e) => setBridePhone(e.target.value)}
                       formControlProps={{
                         fullWidth: true,
                       }}
@@ -110,14 +163,18 @@ export default function LoginPage(props) {
                         type: "text",
                         endAdornment: (
                           <InputAdornment position="end">
-                            <Phone className={classes.inputIconsColor} />
+                          <Phone className={classes.inputIconsColor} />
                           </InputAdornment>
-                        ),
-                      }}
-                    />
-                    <CustomInput
+                          ),
+                        }}
+                      /> */}
+                      <label>Phone Number</label>
+                      <input type="text" placeholder="Phone Number" id="bridePhone" onChange={handlePress} value={bridePhone}/>
+                    {/* <CustomInput
                       labelText="Account info..."
-                      id="account_info"
+                      id="setBrideAccount"
+                      saveInfo={userInfo.brideAccount}
+                      onChange={ (e) => setBrideAccount(e.target.value)}
                       formControlProps={{
                         fullWidth: true,
                       }}
@@ -125,15 +182,19 @@ export default function LoginPage(props) {
                         type: "text",
                         endAdornment: (
                           <InputAdornment position="end">
-                            <Account className={classes.inputIconsColor} />
+                          <Account className={classes.inputIconsColor} />
                           </InputAdornment>
-                        ),
-                      }}
-                    />
+                          ),
+                        }}
+                      /> */}
+                      <label>계좌 번호</label>
+                      <input type="text" placeholder="계좌번호" id="brideAccount" onChange={(e) => {setBrideAccount(e.target.value), console.log("brideAccount"+brideAccount)}}/>
                     <p className={classes.divider}>신랑 측</p>
-                    <CustomInput
+                    {/* <CustomInput
                       labelText="Name..."
-                      id="name"
+                      id="setGroomName"
+                      saveInfo={userInfo.groomName}
+                      onChange={ (e) => setGroomName(e.target.value)}
                       formControlProps={{
                         fullWidth: true,
                       }}
@@ -141,14 +202,17 @@ export default function LoginPage(props) {
                         type: "text",
                         endAdornment: (
                           <InputAdornment position="end">
-                            <People className={classes.inputIconsColor} />
+                          <People className={classes.inputIconsColor} />
                           </InputAdornment>
-                        ),
+                          ),
                       }}
-                    />
-                    <CustomInput
+                    /> */}
+                    <label>신랑 이름</label>
+                    <input type="text" placeholder="이름" id="groomName" onChange={(e) => {setGroomName(e.target.value), console.log("groomName"+groomName)}}></input>
+                    {/* <CustomInput
                       labelText="Phone..."
-                      id="phone"
+                      id="setGroomPhone"
+                      saveInfo={userInfo.groomPhone}
                       formControlProps={{
                         fullWidth: true,
                       }}
@@ -156,15 +220,17 @@ export default function LoginPage(props) {
                         type: "text",
                         endAdornment: (
                           <InputAdornment position="end">
-                            <Phone className={classes.inputIconsColor} />
+                          <Phone className={classes.inputIconsColor} />
                           </InputAdornment>
-                        ),
-                      }}
-                    />
-                    <CustomInput
+                          ),
+                        }}
+                      /> */}
+                      <label>Phone Number</label>
+                      <input type="text" placeholder="Phone Number" id="groomPhone" onChange={handlePress}/>
+                    {/* <CustomInput
                       labelText="Account info..."
-                      id="account-info"
-                   
+                      id="setGroomAccount"
+                      saveInfo={userInfo.groomAccount}
                       formControlProps={{
                         fullWidth: true,
                       }}
@@ -172,18 +238,19 @@ export default function LoginPage(props) {
                         type: "text",
                         endAdornment: (
                           <InputAdornment position="end">
-                            <Account className={classes.inputIconsColor} />
+                          <Account className={classes.inputIconsColor} />
                           </InputAdornment>
-                        ),
-                      }}
-                    />
+                          ),
+                        }}
+                      /> */}
+                      <label>계좌 번호</label>
+                      <input type="text" placeholder="계좌번호" id="groomAccount" onChange={(e) => {setGroomAccount(e.target.value), console.log("groomAccount"+groomAccount)}}></input>
 
-                    <Address />
+                    {/* <Address /> */}
                     
-                    <CustomInput
+                    {/* <CustomInput
                       labelText="Wedding Info..."
                       id="wedding-info"
-                      onClick={() => setClassicModal(true)}
                       formControlProps={{
                         fullWidth: true,
                       }}
@@ -192,11 +259,10 @@ export default function LoginPage(props) {
                         endAdornment: (
                           <InputAdornment position="end">
                             <Info className={classes.inputIconsColor} />
-                            
                           </InputAdornment>
                         ),
                       }} 
-                    />
+                    /> */}
                     
                     
                     <div>
@@ -213,8 +279,8 @@ export default function LoginPage(props) {
                       </GridContainer>
                     </div>{" "}
                     <br />
-                    <UploadImages></UploadImages>
-                    <Address></Address>
+                    {/* <UploadImages></UploadImages> */}
+                    {/* <Address></Address> */}
                   </CardBody>
                   <CardFooter className={classes.cardFooter}>
                     <GridItem xs={12} sm={12} md={6} lg={4}>
@@ -264,6 +330,14 @@ export default function LoginPage(props) {
                             현재 입력하신 정보에 대해 한번더 확인해주시고,
                             맞다면 확인 완료 버튼을 눌러 주세요.
                           </p>
+
+                          <h6>신랑 이름 : {groomName}</h6>
+                          <h6>신부 이름 : {brideName}</h6>
+                          <h6>신랑 전화번호 : {groomPhone}</h6>
+                          <h6>신부 전화번호 : {bridePhone}</h6>
+                          <h6>신랑 계좌 : {groomAccount}</h6>
+                          <h6>신부 계좌 : {brideAccount}</h6>
+
                         </DialogContent>
                         <DialogActions className={classes.modalFooter}>
                           <Button color="transparent" simple>
@@ -281,6 +355,7 @@ export default function LoginPage(props) {
                     </GridItem>
                   </CardFooter>
                 </form>
+                
               </Card>
             </GridItem>
           </GridContainer>
@@ -288,5 +363,7 @@ export default function LoginPage(props) {
         <Footer whiteFont />
       </div>
     </div>
+    </>
   );
 }
+
